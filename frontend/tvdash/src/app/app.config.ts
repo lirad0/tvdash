399c813f8b6046b951fe172604a44596a3c72fe5
lirad0 <292372902+lirad0@users.noreply.github.com>
2026-07-14 18:52:40 +0200

    fix style

--- frontend/tvdash/src/app/app.config.ts
+++ frontend/tvdash/src/app/app.config.ts
@@ -6,7 +6,19 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { providePrimeNG } from 'primeng/config';
 
+import { definePreset } from '@primeuix/themes';
 import Aura from '@primeuix/themes/aura'
+
+const IndigoAura = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}', 100: '{indigo.100}', 200: '{indigo.200}',
+      300: '{indigo.300}', 400: '{indigo.400}', 500: '{indigo.500}',
+      600: '{indigo.600}', 700: '{indigo.700}', 800: '{indigo.800}',
+      900: '{indigo.900}', 950: '{indigo.950}'
+    },
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,12 +27,27 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch()),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: IndigoAura,
         options: {
           darkModeSelector: ".tvdash-dark-theme", // Enables toggle via class (or use false for static dark mode)
           cssLayer: {
             name: 'primeng',
             order: 'theme, base, primeng'
+          },
+          semantic: {
+            primary: {
+              50: '{indigo.50}',
+              100: '{indigo.100}',
+              200: '{indigo.200}',
+              300: '{indigo.300}',
+              400: '{indigo.400}',
+              500: '{indigo.500}',
+              600: '{indigo.600}',
+              700: '{indigo.700}',
+              800: '{indigo.800}',
+              900: '{indigo.900}',
+              950: '{indigo.950}'
+            }
           }
         }
       }
